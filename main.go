6db54cf1c74114dfa986f1c246374d91886f1f48
Trev-D-Dev <dunn.trevor@pm.me>
2025-07-02 02:24:21 -0400

    Created REPL loop

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if scanner.Scan() {
+			input := scanner.Text()
+
+			cInput := cleanInput(input)
+
+			word1 := cInput[0]
+
+			fmt.Printf("Your command was: %v\n", word1)
+		}
+	}
 }
